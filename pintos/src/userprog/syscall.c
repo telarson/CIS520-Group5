84bf56d3801df353342ba607869b2777df18f958
Michael Brinkman <mbrinkman1026@gmail.com>
2021-03-27 14:09:37 -0500

    add method to write to user memory

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -18,6 +18,7 @@
 static void syscall_handler (struct intr_frame *);
 static int read_usr_stack (void *init_addr, void *result, size_t num_of_bytes);
 static int get_user (const uint8_t *uaddr);
+static bool put_user (uint8_t *udst, uint8_t byte);
 struct file* get_file(int fd);
 
 
@@ -210,6 +211,18 @@ get_user (const uint8_t *uaddr){
   asm ("movl $1f, %0; movzbl %1, %0; 1:"
    : "=&a" (result) : "m" (*uaddr));
   return result;
+}
+
+/* Writes BYTE to user address UDST.
+   UDST must be below PHYS_BASE.
+   Returns true if successful, false if a segfault occurred.
+*/
+static bool
+put_user (uint8_t *udst, uint8_t byte){
+  int error_code;
+  asm ("movl $1f, %0; movb %b2, %1; 1:"
+    : "=&a" (error_code), "=m" (*udst) : "q" (byte));
+  return error_code != -1;
 }
 
 void halt (void)
